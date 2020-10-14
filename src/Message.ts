6cfd39114f904476c8c5b91b50598f318a736442
Egor Shulga <egorshulga@outlook.com>
2020-10-14 12:09:25 +0300

    feat: add message event emitter

--- src/Message.ts
+++ src/Message.ts
@@ -1,7 +1,9 @@
-import { NativeModules } from 'react-native'
+import { NativeEventEmitter, NativeModules } from 'react-native'
 import type { SamsungAccessoryAgent } from './Agent'
 
 export const SAMessage: SamsungAccessoryMessage = NativeModules.SAMessage
+
+export const SAMessageEvents = new NativeEventEmitter(NativeModules.SAMessage)
 
 export interface SamsungAccessoryMessage extends SamsungAccessoryAgent {
   sendMessage(message: string): Promise<void>;
